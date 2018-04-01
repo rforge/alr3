986c9fcc96d3a2ab4263f871ae993c21ddea9e63
sanfordweisberg <sanfordweisberg@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2018-04-01 21:24:57 +0000

    trivial change
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/alr3@29 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/alr3.R
+++ pkg/R/alr3.R
@@ -9,11 +9,11 @@ pureErrorAnova <- function(mod){UseMethod("pureErrorAnova")}
 pureErrorAnova.lm <- function(mod) {
  if (is.null(mod$model)) mod <- update(mod, model=TRUE)
  p <- dim(mod$model)[2] -1
- mod$model$Lack.of.Fit <- 
+ mod$model$Lack.of.Fit <-
    factor(randomLinComb(model.matrix(mod), 101319853))
  aov1 <- anova(mod)
  #set.seed(save.seed) # restore random number seed
- if (length(levels(mod$model$Lack.of.Fit)) == length(mod$model$Lack.of.Fit)) 
+ if (length(levels(mod$model$Lack.of.Fit)) == length(mod$model$Lack.of.Fit))
   aov1 else {
   aov2 <- anova(lm(mod$model[ , 1]~mod$model$Lack.of.Fit, weights=weights(mod)))
   rrow <- dim(aov1)[1]
@@ -28,20 +28,20 @@ pureErrorAnova.lm <- function(mod) {
   aov
   }}
 
- 
+
 randomLinComb <- function(X, seed=NULL) {UseMethod("randomLinComb")}
 
 randomLinComb.default <- function(X, seed=NULL) {
  if(!is.null(seed)) set.seed(seed)
- std <- function(x){ 
+ std <- function(x){
     s <- sd(x)
     if( s > 0) (x-mean(x))/s else x}
  as.vector(apply(X, 2, std)%*% as.vector(2*rnorm(dim(X)[2])-1) )
  }
- 
+
 randomLinComb.lm <- function(X, ...) {
   randomLinComb(model.matrix(X), ...)}
- 
+
 randomLinComb.lm <- function(X, seed=NULL) {
  if(is.null(X$model)) X <- update(X, model=TRUE)
  randomLinComb(X$model[ , -1], seed=seed)}
@@ -56,7 +56,7 @@ randomLinComb.lm <- function(X, seed=NULL) {
 # pod models; Cook and Weisberg (2004), American Statistician
 ##################################################################
 
-# This code was written by Lexin Li.  It was modified and simplified 
+# This code was written by Lexin Li.  It was modified and simplified
 # by S. Weisberg to work with standard R stuff in nls.
 
 pod <- function(x, ...) {UseMethod("pod")}
@@ -65,33 +65,33 @@ pod.lm <- function(x, group, mean.function, control, ...) {
  call <- x$call
  call[[1]] <- as.name("pod")
  if(!missing(group)) call$group <- as.name(as.character(substitute(group)))
- if(!missing(mean.function)) call$mean.function <- 
+ if(!missing(mean.function)) call$mean.function <-
      as.character(substitute(mean.function))
- if(!missing(control)) call$control <- 
+ if(!missing(control)) call$control <-
      as.name(as.character(substitute(control)))
  eval(call)}
 
 pod.formula <-
-function (formula, data=sys.parent(), group, subset, weights=NULL, na.action, 
-    mean.function=c("pod", "common", "parallel", "general"), 
-    singular.ok = FALSE, contrasts = NULL, offset, control=nls.control(), ...) 
-{ 
+function (formula, data=sys.parent(), group, subset, weights=NULL, na.action,
+    mean.function=c("pod", "common", "parallel", "general"),
+    singular.ok = FALSE, contrasts = NULL, offset, control=nls.control(), ...)
+{
     op <- options(warn=-1)  # suppress warnings
     mod <- match.arg(mean.function)
     call <- match.call(expand.dots=FALSE)
     call$... <- NULL
     subset <- if (missing(subset)) NULL else call$subset
     g <- factor(eval(substitute(group), data))
-    gname <- substitute(group)  
+    gname <- substitute(group)
     gname <- as.character(if(length(gname) == 1) gname else gname[3])
-    if (mod != "pod") { 
+    if (mod != "pod") {
       if (mod == "common") {
        call$group <- call$mean.function <- NULL
        call[[1]] <- as.name("lm")
        ans <- eval(call, parent.frame())} else
      if (mod == "parallel") {
        assign(gname, g)
-       call$formula <- update(as.formula(call$formula), 
+       call$formula <- update(as.formula(call$formula),
                        as.formula(paste("~.+", gname, sep="")))
                         #as.formula("~.+g"))
        call$group <- call$mean.function <- NULL
@@ -99,21 +99,21 @@ function (formula, data=sys.parent(), group, subset, weights=NULL, na.action,
        ans <- eval(call)} else
      if (mod == "general") {
        assign(gname, g)
-       call$formula <- update(as.formula(call$formula), 
+       call$formula <- update(as.formula(call$formula),
                         as.formula(paste("~(.)*", gname, sep="")))
        call$group <- call$mean.function <- NULL
        call[[1]] <- as.name("lm")
        ans <- eval(call)}
      ans$pod.mean.function <- mod
-     ans$group <- structure(if (!is.null(subset)) g[eval(subset)] else g,  
+     ans$group <- structure(if (!is.null(subset)) g[eval(subset)] else g,
                             name=gname)
      class(ans) <- c("pod.lm", "lm")
      ans} else
     { # POD model
-    l<-nlevels(g) 
+    l<-nlevels(g)
     cl <- call
     cl$group <- cl$mean.function <- NULL
-    cl[[1]] <- as.name("lm") 
+    cl[[1]] <- as.name("lm")
     fit1 <- eval(cl)
     if(fit1$rank != length(fit1$coef))
      stop("Predictors are linearly related.  Try again after deleting predictors.")
@@ -121,7 +121,7 @@ function (formula, data=sys.parent(), group, subset, weights=NULL, na.action,
     p1 <- predict(fit1) - coef(fit1)[1]
 # If subset is not NULL, find the subscripts of the cases used:
     if(!is.null(call$subset)){
-       rows <- row.names(model.matrix(update(fit1, subset=NULL))) %in% 
+       rows <- row.names(model.matrix(update(fit1, subset=NULL))) %in%
                row.names(model.matrix(fit1))
        } else {
        rows <- rep(TRUE, dim(model.matrix(fit1))[1])}
@@ -135,19 +135,19 @@ function (formula, data=sys.parent(), group, subset, weights=NULL, na.action,
     if (is.null(cl$data)){
        cl$data <- data.frame(p1=p1, g=g)} else {
        cl$data <- data.frame(data, p1=p1, g=g)}
-# fit parallel lines model  
+# fit parallel lines model
     fit2 <- eval(cl)
     coef2 <- coef(fit2)
 # group.data.subset excludes the subset
-    group.data.subset <- 
+    group.data.subset <-
               model.matrix(fit2$terms, fit2$model, contrasts)[ , 2+1:(l-1)]
     group.data.subset <- data.frame(group=group.data.subset)
 # again, fix for non-null subset
     if(!is.null(subset)){
-       group.data <- data.frame(matrix(NA, nrow=bign, 
+       group.data <- data.frame(matrix(NA, nrow=bign,
           ncol=dim(group.data.subset)[2]))
        group.data[rows, ] <- group.data.subset} else {
-       group.data <- group.data.subset}    
+       group.data <- group.data.subset}
 # construct new data with X, y, and G
     formula<-as.formula(formula)
     y.name<-as.character(attr(fit1$terms, "predvars")[2])
@@ -162,33 +162,33 @@ function (formula, data=sys.parent(), group, subset, weights=NULL, na.action,
     for(i in 2:p) {
        eta<-paste("eta", i, sep="")
        form1<-paste(form1, "+", eta, "*", x.name[i])
-       para.name<-c(para.name, eta)  
-    } 
+       para.name<-c(para.name, eta)
+    }
    form2<-paste("eta0 +", form1)
    for(i in 1:(l-1)) {
       G<-g.name[i]
       th0<-paste("th0", i+1, sep="")
       th1<-paste("th1", i+1, sep="")
-      form2<-paste(form2, "+", G, "* (", th0, "+", th1, "* (", form1, "))") 
+      form2<-paste(form2, "+", G, "* (", th0, "+", th1, "* (", form1, "))")
       para.name<-c(para.name, c(th0, th1))
-   } 
+   }
 # New June 6, 2005
    if (is.null(weights))
       {form<-as.formula(paste(y.name, "~", form2))} else
       {wts <- substitute(weights)
-       form<-as.formula(paste("sqrt(", wts, ")*", y.name, "~", 
+       form<-as.formula(paste("sqrt(", wts, ")*", y.name, "~",
              "sqrt(", wts, ")*(", form2, ")", sep=""))}
 # End change
-   start <- c(coef2[1], coef2[2]*coef1[-1], coef2[3:(l+1)], 
+   start <- c(coef2[1], coef2[2]*coef1[-1], coef2[3:(l+1)],
                 coef2[(l+2):(2*l)]/coef2[2])
-   names(start) <- c(paste("eta", 0:(length(x.name)), sep=""), 
+   names(start) <- c(paste("eta", 0:(length(x.name)), sep=""),
                      paste("th0", 2:l, sep=""), paste("th1", 2:l, sep=""))
    if (is.null(call$data)){
         for (j in 1:length(g.name)) assign(g.name[j], group.data[ , j])
-        obj.nl<-nls(form, start=start, subset=rows, 
+        obj.nl<-nls(form, start=start, subset=rows,
             control=control, ...)} else {
-        obj.nl<-nls(form, cbind(data, group.data), start=start, 
-            subset=rows, control=control, ...)}  
+        obj.nl<-nls(form, cbind(data, group.data), start=start,
+            subset=rows, control=control, ...)}
 # evaluate linear part, but only for the subset of cases used
    eta.name<-para.name[2:(p+1)]
    eta.value<-coef(obj.nl)[2:(p+1)]
@@ -215,7 +215,7 @@ function (formula, data=sys.parent(), group, subset, weights=NULL, na.action,
 print.pod<-        function(x, ...){ print(x$nls.fit) }
 summary.pod <-     function(object, ...){ summary(object$nls.fit, ...) }
 coef.pod <-        function(object, ...){ coef(object$nls.fit, ...)}
-deviance.pod <-    function(object, ...){ deviance(object$nls.fit, ...)} 
+deviance.pod <-    function(object, ...){ deviance(object$nls.fit, ...)}
 vcov.pod <-        function(object, ...){ vcov(object$nls.fit, ...)}
 residuals.pod <-   function(object, ...){ residuals(object$nls.fit)}
 formula.pod<-      function(x, ...){      formula(x$call)}
@@ -225,8 +225,8 @@ df.residual.pod <- function(object, ...){
 predict.pod     <- function(object, ...){ predict(object$nls.fit)}
 
 # Plot one dimensional models by group
-plot.pod.lm <- function(x, colors=1:nlevels(x$group), 
-      pch=1:nlevels(x$group), key="topleft", identify=FALSE, 
+plot.pod.lm <- function(x, colors=1:nlevels(x$group),
+      pch=1:nlevels(x$group), key="topleft", identify=FALSE,
       xlab="Linear Predictor", ylab=as.character(c(formula(x)[[2]])), ...) {
   mean.function <- x$pod.mean.function
   if(mean.function == "general") stop("No 2D plot for the general pod model")
@@ -238,43 +238,43 @@ plot.pod.lm <- function(x, colors=1:nlevels(x$group),
   gname <- as.character(x$call[[gloc]])
 # common regressions
   if(mean.function == "common"){
-    plot(predict(x), x$model[ , 1], ylab=ylab, pch=pch[g1], col=colors[g1], 
+    plot(predict(x), x$model[ , 1], ylab=ylab, pch=pch[g1], col=colors[g1],
     xlab=paste(xlab, ", ignore groups", sep=""), ...)
     abline(lm(x$model[ , 1]~predict(x)))}
 # parallel regressions
   if(mean.function == "parallel"){
       c2 <- coef(x)
       xp <-0
-      for (j in 2:(length(c2)-nlevels(x$group)+1)) 
+      for (j in 2:(length(c2)-nlevels(x$group)+1))
          xp <- xp +c2[j] * model.matrix(x)[ , j]
-      plot(xp, x$model[ , 1], pch=pch[g1], col=colors[g1], 
-       ylab=paste(ylab, ", Groups = ", g1.name, sep=""), 
+      plot(xp, x$model[ , 1], pch=pch[g1], col=colors[g1],
+       ylab=paste(ylab, ", Groups = ", g1.name, sep=""),
        xlab=paste(xlab,  ", parallel mean function", sep=""), ...)
       for (j in 1:nlevels(x$group))
-       abline(if(j==1) c2[1] else c2[1]+c2[length(c2)-nlevels(x$group)+j], 1, 
+       abline(if(j==1) c2[1] else c2[1]+c2[length(c2)-nlevels(x$group)+j], 1,
            lty=j, col=colors[j])}
 # key
   if (class(key) == "logical") {
    if (key == TRUE) {
       print("Click mouse on plot to locate the key, or press Escape")
-      loc <-locator(n=1) 
-      legend(loc[1], loc[2], legend = as.character(levels(x$group)), 
-           lty=1:nlevels(x$group), col=colors[1:nlevels(x$group)], 
+      loc <-locator(n=1)
+      legend(loc[1], loc[2], legend = as.character(levels(x$group)),
+           lty=1:nlevels(x$group), col=colors[1:nlevels(x$group)],
            pch=pch[1:nlevels(x$group)])}}
-   else { 
+   else {
       loc <- key
-      legend(loc[1], loc[2], legend = as.character(levels(x$group)), 
-           lty=1:nlevels(x$group), col=colors[1:nlevels(x$group)], 
+      legend(loc[1], loc[2], legend = as.character(levels(x$group)),
+           lty=1:nlevels(x$group), col=colors[1:nlevels(x$group)],
            inset=0.01, pch=pch[1:nlevels(x$group)])}
 # identify
   if(identify == TRUE){
       identify(xp, x$model[ , 1], row.names(x$model))}
   invisible()}
 
-plot.pod <-function(x, colors=1:nlevels(x$group), 
-  pch=1:nlevels(x$group), key="topleft", identify=FALSE, 
+plot.pod <-function(x, colors=1:nlevels(x$group),
+  pch=1:nlevels(x$group), key="topleft", identify=FALSE,
   xlab="Linear Predictor", ylab=as.character(c(formula(x)[[2]])), ...)
-{ 
+{
    yp<-residuals(x)+fitted(x)
    group <- x$group
    gloc <- match("group", names(x$call))
@@ -283,56 +283,55 @@ plot.pod <-function(x, colors=1:nlevels(x$group),
    levels(g1) <- 1:nlevels(group)
    g1 <- as.numeric(as.character(g1))
    gname <- as.character(x$call[[gloc]])
-   plot(xp, yp, pch=pch[g1], col=colors[g1], 
-    ylab=paste(ylab, ", Groups = ", as.character(x$call$group), sep=""), 
+   plot(xp, yp, pch=pch[g1], col=colors[g1],
+    ylab=paste(ylab, ", Groups = ", as.character(x$call$group), sep=""),
     xlab=paste(xlab,  ", pod mean function", sep=""), ...)
       for (j in 1:nlevels(group))
         {abline(lm(yp~xp, subset=g1==j), lty=j, col=colors[j])}
    if (class(key) == "logical") {
    if (key == TRUE) {
       print("Click mouse on plot to locate the key, or press Escape")
-      loc <-locator(n=1) 
-      legend(loc[1], loc[2], legend = as.character(levels(group)), 
-           lty=1:nlevels(group), col=colors[1:nlevels(group)], 
+      loc <-locator(n=1)
+      legend(loc[1], loc[2], legend = as.character(levels(group)),
+           lty=1:nlevels(group), col=colors[1:nlevels(group)],
            pch=pch[1:nlevels(group)])}} else
-    { 
+    {
       loc <- key
-      legend(loc[1], loc[2], legend = as.character(levels(group)), 
-           lty=1:nlevels(group), col=colors[1:nlevels(group)], 
+      legend(loc[1], loc[2], legend = as.character(levels(group)),
+           lty=1:nlevels(group), col=colors[1:nlevels(group)],
            inset=0.01, pch=pch[1:nlevels(group)])}
    invisible()
 }
 
-anova.pod <- 
-function (object, scale = 0, test = "F", ...) 
-{   
+anova.pod <-function (object, scale = 0, test = "F", ...)
+{
     m1 <- update(object, mean.function="common")
     m2 <- update(object, mean.function="parallel")
     m4 <- update(object, mean.function="general")
     objects <- list(m1, m2, object, m4)
     resdf  <- as.numeric(lapply(objects, df.residual))
     resdev <- as.numeric(lapply(objects, deviance))
-    table <- data.frame(resdf, resdev, c(NA, -diff(resdf)), c(NA, 
+    table <- data.frame(resdf, resdev, c(NA, -diff(resdf)), c(NA,
         -diff(resdev)))
     variables <- c("1: common", "2: parallel", "3: pod", "4: pod + 2fi")
-    dimnames(table) <- list(variables, c("Res.Df", "RSS", "Df", 
+    dimnames(table) <- list(variables, c("Res.Df", "RSS", "Df",
         "Sum of Sq"))
-    title <- paste("POD Analysis of Variance Table for ", 
-            deparse(formula(objects[[1]])[[2]]), ", grouped by ", 
+    title <- paste("POD Analysis of Variance Table for ",
+            deparse(formula(objects[[1]])[[2]]), ", grouped by ",
             as.character(object$call$group), "\n" , sep="")
-    topnote <- c(paste("1: ", deparse(formula(objects[[1]])), sep=""), 
-                 paste("2: ", deparse(formula(objects[[2]])), sep=""), 
-                 paste("3: ", deparse(formula(object$nls.fit)), sep=""), 
+    topnote <- c(paste("1: ", deparse(formula(objects[[1]])), sep=""),
+                 paste("2: ", deparse(formula(objects[[2]])), sep=""),
+                 paste("3: ", deparse(formula(object$nls.fit)), sep=""),
                  paste("4: ", deparse(formula(objects[[4]])), sep=""))
     if (!is.null(test)) {
         bigmodel <- order(resdf)[1]
-        scale <- if (scale > 0) 
+        scale <- if (scale > 0)
             scale
         else resdev[bigmodel]/resdf[bigmodel]
-        table <- stat.anova(table = table, test = test, scale = scale, 
+        table <- stat.anova(table = table, test = test, scale = scale,
             df.scale = resdf[bigmodel], n = length(objects[bigmodel$residuals]))
     }
-    structure(table, heading = c(title, topnote), class = c("anova", 
+    structure(table, heading = c(title, topnote), class = c("anova",
         "data.frame"))
 }
 
@@ -347,14 +346,14 @@ function (object, scale = 0, test = "F", ...)
 
 ##########################################################################
 # replaced by 'powerTransform' in 'car'
-    
-    
+
+
 ################################################################################
 #    Chapter 8
 ################################################################################
 # Test for curvature in a residual plot (Chapter 8)
 # Residual plots, and curvature tests in 'car'
-  
+
 #resid.curv.test = residCurvTest in 'car'
 
 #resplot = residualPlot in 'car'
